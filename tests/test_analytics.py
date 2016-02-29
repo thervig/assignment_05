b9a2f7e65562fc90bfdfb25dfbf729fe4fd474ab
thervig <thervig@asu.edu>
2016-02-29 13:52:59 -0700

    Update test_analytics.py

--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -11,7 +11,8 @@ class TestAnalytics(unittest.TestCase):
         pass
     
     def test_permutations(self):
-        self.assertEqual(len(analytics.permutations(99), 99)
+        self.assertEqual(len(analytics.permutations(100), 100)
         
     def test_critical_points(self):
-        criticals = analytics.critical_points( 
+        criticals = analytics.critical_points([1, 2, 3, 4, 5])
+        self.assterTrue(criticals(min) == 1 and criticals(max) == 5
