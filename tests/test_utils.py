f829fc84dd6f404ce8126ba7f380e74f3962abeb
thervig <thervig@asu.edu>
2016-02-28 20:33:27 -0700

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,3 +9,10 @@ class TestUtils(unittest.TestCase):
 
     def setUp(self):
         pass
+    
+    def generate_random_test(self):
+        self.assertEqual(len(utils.generate_random(1000)), 1000)
+    
+    def significant_distance_test(self):
+        self.assertFalse(utils.significant_distance(9.9, 30, 10))
+        self.assertTrue(utils.significant_distance(10, 30, 9.9))
