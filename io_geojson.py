81ba3308cc549b2ec3c70031372b862417d0da06
thervig <thervig@asu.edu>
2016-02-28 20:08:19 -0700

    Update io_geojson.py

---
+++ io_geojson.py
@@ -0,0 +1,20 @@
+import json
+
+def read_geojson(input_file):
+    """
+    Read a geojson file
+    Parameters
+    ----------
+    input_file : str
+                 The PATH to the data to be read
+    Returns
+    -------
+    gj : dict
+         An in memory version of the geojson
+    """
+    # Please use the python json module (imported above)
+    # to solve this one.
+    with open ('data/us_cities.geojson', 'r') as f:
+        gj = json.load(f)
+    
+    return gj
