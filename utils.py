d33fe1e881d1e526ce826d1558d1b1d3ab1d08af
thervig <thervig@asu.edu>
2016-02-28 17:08:53 -0700

    Update utils.py

---
+++ utils.py
@@ -0,0 +1,250 @@
+import math
+import random
+
+
+def generate_random(n):
+   """
+   Generates n random points within the domain of 0 - 1.
+   :param n:
+   :return:
+   """
+   points = random.Random()
+   points_list = []
+   for i in range(n):
+       points_list.append((round(points.uniform(0, 1), 2), round(points.uniform(0, 1), 2)))
+   return (points_list)
+
+
+def significant_distance(less, greater, distance):
+    return (less > distance) or (greater < distance)
+
+
+def mean_center(points):
+    """
+    Given a set of points, compute the mean center
+    Parameters
+    ----------
+    points : list
+         A list of points in the form (x,y)
+    Returns
+    -------
+    x : float
+        Mean x coordinate
+    y : float
+        Mean y coordinate
+    """
+
+    summation_x = 0
+    summation_y = 0
+    for i in points:
+        summation_x += i[0]
+        summation_y += i[1]
+
+    x = float(summation_x / len(points))
+    y = float(summation_y / len(points))
+
+    return x, y
+
+
+def minimum_bounding_rectangle(points):
+    """
+    Given a set of points, compute the minimum bounding rectangle.
+    Parameters
+    ----------
+    points : list
+             A list of points in the form (x,y)
+    Returns
+    -------
+     : list
+       Corners of the MBR in the form [xmin, ymin, xmax, ymax]
+    """
+
+    mbr = [0,0,0,0]
+    xmin = 999
+    ymin = 999
+    xmax = 0
+    ymax = 0
+    for i in points:
+        if i[0] < xmin:
+            xmin = i[0]
+        if i[0] > xmax:
+            xmax = i[0]
+        if i[1] < ymin:
+            ymin = i[1]
+        if i[1] > ymax:
+            ymax = i[1]
+
+    mbr = [xmin, ymin, xmax, ymax]
+
+    return mbr
+
+
+def mbr_area(mbr):
+    """
+    Compute the area of a minimum bounding rectangle
+    """
+    area = (mbr[2] - mbr[0]) * (mbr[3] - mbr[1])
+
+    return area
+
+
+def expected_distance(area, n):
+    """
+    Compute the expected mean distance given
+    some study area.
+    This makes lots of assumptions and is not
+    necessarily how you would want to compute
+    this.  This is just an example of the full
+    analysis pipe, e.g. compute the mean distance
+    and the expected mean distance.
+    Parameters
+    ----------
+    area : float
+           The area of the study area
+    n : int
+        The number of points
+    """
+
+    expected = .5 / ((math.sqrt(n/area)))
+    return expected
+
+
+"""
+Below are the functions that you created last week.
+Your syntax might have been different (which is awesome),
+but the functionality is identical.  No need to touch
+these unless you are interested in another way of solving
+the assignment
+"""
+
+def manhattan_distance(a, b):
+    """
+    Compute the Manhattan distance between two points
+    Parameters
+    ----------
+    a : tuple
+        A point in the form (x,y)
+    b : tuple
+        A point in the form (x,y)
+    Returns
+    -------
+    distance : float
+               The Manhattan distance between the two points
+    """
+    distance =  abs(a[0] - b[0]) + abs(a[1] - b[1])
+    return distance
+
+
+def euclidean_distance(a, b):
+    """
+    Compute the Euclidean distance between two points
+    Parameters
+    ----------
+    a : tuple
+        A point in the form (x,y)
+    b : tuple
+        A point in the form (x,y)
+    Returns
+    -------
+    distance : float
+               The Euclidean distance between the two points
+    """
+    distance = math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+    return distance
+
+
+def shift_point(point, x_shift, y_shift):
+    """
+    Shift a point by some amount in the x and y directions
+    Parameters
+    ----------
+    point : tuple
+            in the form (x,y)
+    x_shift : int or float
+              distance to shift in the x direction
+    y_shift : int or float
+              distance to shift in the y direction
+    Returns
+    -------
+    new_x : int or float
+            shited x coordinate
+    new_y : int or float
+            shifted y coordinate
+    Note that the new_x new_y elements are returned as a tuple
+    Example
+    -------
+    >>> point = (0,0)
+    >>> shift_point(point, 1, 2)
+    (1,2)
+    """
+    x = getx(point)
+    y = gety(point)
+
+    x += x_shift
+    y += y_shift
+
+    return x, y
+
+
+def check_coincident(a, b):
+    """
+    Check whether two points are coincident
+    Parameters
+    ----------
+    a : tuple
+        A point in the form (x,y)
+    b : tuple
+        A point in the form (x,y)
+    Returns
+    -------
+    equal : bool
+            Whether the points are equal
+    """
+    return a == b
+
+
+def check_in(point, point_list):
+    """
+    Check whether point is in the point list
+    Parameters
+    ----------
+    point : tuple
+            In the form (x,y)
+    point_list : list
+                 in the form [point, point_1, point_2, ..., point_n]
+    """
+    return point in point_list
+
+
+def getx(point):
+    """
+    A simple method to return the x coordinate of
+     an tuple in the form(x,y).  We will look at
+     sequences in a coming lesson.
+    Parameters
+    ----------
+    point : tuple
+            in the form (x,y)
+    Returns
+    -------
+     : int or float
+       x coordinate
+    """
+    return point[0]
+
+
+def gety(point):
+    """
+    A simple method to return the x coordinate of
+     an tuple in the form(x,y).  We will look at
+     sequences in a coming lesson.
+    Parameters
+    ----------
+    point : tuple
+            in the form (x,y)
+    Returns
+    -------
+     : int or float
+       y coordinate
+    """
+    return point[1]
